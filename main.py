949adf393941d881825a6ce253fb9967c7d0929d
Jorge Morales <moralesjorge878@gmail.com>
2022-05-03 09:42:29 -0600

    datos y oy

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Nombre del estudiante: Jorge Luis Morales Huertas")
+print("Carné del estudiante: 1586322")
+print("Carrera: Ingeniería en informática y sistemas")
+print("Facultad: Ingeniería")
+print("Curso: introducción a la ingeniería en informática y sistemas")
+
