f9260f3e82dcc1da1f70954885a16ed1a105e969
Jorge Morales <moralesjorge878@gmail.com>
2022-05-03 09:49:45 -0600

    rama main

--- main.py
+++ main.py
@@ -4,3 +4,13 @@ print("Carrera: Ingeniería en informática y sistemas")
 print("Facultad: Ingeniería")
 print("Curso: introducción a la ingeniería en informática y sistemas")
 
+# ---------------------------
+
+print("Programa de animal")
+print("Animal favorito: Oso polar")
+animal = input("Ingrese su animal favorito")
+if animal != "Oso polar":
+    print("Es un animal interesante, pero me gustan más los Osos polares")
+if animal == "Oso polar":
+    print("A mí también me gustan los osos polares")
+    
